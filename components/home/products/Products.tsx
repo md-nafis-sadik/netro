8ac5bf97d7c16d3b211dd53fa7ba7ec39375f323
hasibulofcs <hasibulofcs@gmail.com>
2025-01-23 18:30:11 +0600

    updated container issues

--- components/home/products/Products.tsx
+++ components/home/products/Products.tsx
@@ -8,24 +8,26 @@ import colors from "@/lib/colors";
 
 const Products = () => {
   return (
-    <section className="containerX bg-black flex_center flex-col w-full py-20 overflow-hidden">
-      <SectionSubHeader dark text="Our Products" />
-      <SectionHeader lite text="Our Innovative" className="mt-5 md:mt-10" />
-      <SectionHeader lite text="Global Products" />
+    <section className="bg-black w-full py-20 overflow-hidden">
+      <div className="containerX flex_center flex-col">
+        <SectionSubHeader dark text="Our Products" />
+        <SectionHeader lite text="Our Innovative" className="mt-5 md:mt-10" />
+        <SectionHeader lite text="Global Products" />
 
-      <div className="max-w-[952px] w-fit mx-auto grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-[60px] my-10 md:my-20">
-        {productsData.map(({ name, description }, index) => (
-          <ProductCard name={name} description={description} key={index} />
-        ))}
+        <div className="max-w-[952px] w-fit mx-auto grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-[60px] my-10 md:my-20">
+          {productsData.map(({ name, description }, index) => (
+            <ProductCard name={name} description={description} key={index} />
+          ))}
+        </div>
+
+        <ArrowLineupButton
+          lineColor={colors.secondary[300]}
+          textClassName="text-white"
+          className="mt-10 md:mt-20"
+        >
+          See all products
+        </ArrowLineupButton>
       </div>
-
-      <ArrowLineupButton
-        lineColor={colors.secondary[300]}
-        textClassName="text-white"
-        className="mt-10 md:mt-20"
-      >
-        See all products
-      </ArrowLineupButton>
     </section>
   );
 };
