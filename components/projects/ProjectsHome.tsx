f7e2024b0a89905541b56c67899d1669d212f10c
hasibulofcs <hasibulofcs@gmail.com>
2025-01-26 08:43:29 +0600

    project section fixes

--- components/projects/ProjectsHome.tsx
+++ components/projects/ProjectsHome.tsx
@@ -5,7 +5,7 @@ import ProjectCard from "./ProjectHomeCard";
 
 const ProjectsHome = () => {
   return (
-    <section className="bg-black w-full py-20 overflow-hidden">
+    <section className="bg-black w-full py-20 relative">
       <div className="containerX flex_center flex-col">
         <SectionSubHeader dark text="Explore Projects" />
         <SectionHeader
