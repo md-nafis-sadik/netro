b8e685c10a19635119d72be8dfccc4b07207d5ac
md-nafis-sadik <sadikmdnafis@gmail.com>
2025-08-21 15:36:44 +0600

    services and more projects

--- components/projects/DiscoverMoreProjects.tsx
+++ components/projects/DiscoverMoreProjects.tsx
@@ -21,6 +21,16 @@ const DiscoverMoreProjects = ({ data }: any) => {
       <div className="containerX w-full overflow-hidden">
         <div className="w-full" ref={emblaRef}>
           <div className="w-full min-h-fit flex flex-row gap-6 md:gap-10 py-10 md:py-20">
+            {/* {data?.map((item: any, index: number) => (
+              <ProjectCard
+                className={cn(
+                  "!min-w-[380px] md:!min-w-[621px] !h-[220px] md:!h-[361px] !max-h-[361px]"
+                )}
+                projectDescriptionClassName={"!line-clamp-1 md:!line-clamp-5"}
+                item={item}
+                key={index}
+              />
+            ))} */}
             {data?.map((item: any, index: number) => (
               <OtherProductCard item={item} key={index} />
             ))}
