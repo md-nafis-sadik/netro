3a2a1797dca6908acefddb29fdde90f10a8fdbff
hridoyhaque01 <hridoyhaque.info@gmail.com>
2026-01-24 18:23:54 +0600

    refactor: simplify TeamHomeWrapper component by removing data fetching logic

--- components/team/TeamHomeWrapper.tsx
+++ components/team/TeamHomeWrapper.tsx
@@ -1,12 +1,5 @@
-import { fetchWithDelay } from "@/lib/apiHandler";
-import Team from "./Team";
 import { ReactNode } from "react";
-import {
-  FacebookIcon,
-  LinkedinOutlinedIcon,
-  TwitterIcon,
-} from "@/services/assets/svgs";
-import colors from "@/lib/colors";
+import Team from "./Team";
 
 // Your TeamMember type
 type TeamMember = {
@@ -21,58 +14,8 @@ type TeamMember = {
 };
 
 // Wrapper component
-const TeamHomeWrapper = async () => {
-  // Step 1: fetch data
-  const response: unknown = await fetchWithDelay(`/teams/all`);
-
-  // Step 2: type guard to ensure response has `data` array
-  const isValidResponse = (obj: any): obj is { data: any[] } =>
-    obj && typeof obj === "object" && Array.isArray(obj.data);
-
-  const teamData: TeamMember[] = isValidResponse(response)
-    ? response.data
-        .map((member: any) => ({
-          name: member.name,
-          position: member.designation,
-          image: member.image,
-          socials: [
-            member.facebook && {
-              type: "facebook",
-              link: member.facebook,
-              icon: (
-                <FacebookIcon
-                  color={colors.black}
-                  className="!h-[14px] !w-[14px] !shrink-0"
-                />
-              ),
-            },
-            member.twitter && {
-              type: "twitter",
-              link: member.twitter,
-              icon: (
-                <TwitterIcon
-                  color={colors.black}
-                  className="!h-[14px] !w-[14px] !shrink-0"
-                />
-              ),
-            },
-            member.linkedin && {
-              type: "linkedin",
-              link: member.linkedin,
-              icon: (
-                <LinkedinOutlinedIcon
-                  color={colors.black}
-                  className="!h-[14px] !w-[14px] !shrink-0"
-                />
-              ),
-            },
-          ].filter(Boolean) as TeamMember["socials"],
-          order: member.order,
-        }))
-        .sort((a, b) => a.order - b.order)
-    : [];
-
-  return <Team teamData={teamData} />;
+const TeamHomeWrapper = () => {
+  return <Team />;
 };
 
 export default TeamHomeWrapper;
