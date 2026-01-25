b20c340cf42774afa0938e8ecd28247480bc0e5f
hridoyhaque01 <hridoyhaque.info@gmail.com>
2026-01-25 18:20:51 +0600

    fix: dashed icons

--- components/projects/CarrierFlowSection.tsx
+++ components/projects/CarrierFlowSection.tsx
@@ -1,26 +1,25 @@
 "use client";
 
-import { ArrowPointerIcon, DashedCurvePathDesktop, DashedCurvePathMobile } from "@/services/assets/svgs";
+import {
+  DashedCurvePointerDesktop,
+  DashedCurvePointerMobile,
+} from "@/services/assets/svgs";
 
 const CarrierFlowSection = ({ items }: any) => {
   return (
     <div className="pt-20">
-      <div className="w-full mx-auto relative">
+      <div className="max-w-[1176px] w-full mx-auto relative">
         {/* SVG for the curve path */}
-        <div className="absolute left-1/2 -translate-x-1/2 top-20">
-          <div className="relative w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] xl:w-[423px]">
-            {/* Desktop version */}
-            <DashedCurvePathDesktop className="hidden sm:block w-full h-auto" />
+        {/* <div className="absolute left-0 sm:left-1/2 sm:-translate-x-1/2 sm:top-20 w-full h-full">
+          <div className="relative  sm:w-[250px] md:w-[300px] lg:w-[350px] xl:w-[423px]">
             
-            {/* Mobile version */}
-            <DashedCurvePathMobile className="block sm:hidden w-full h-auto" />
-
-            {/* Pointer that follows the path */}
-            <span className="absolute top-[42%] left-[55%] pointer-events-none z-2">
-              <ArrowPointerIcon className="w-[24px] sm:w-[30px] md:w-[36px] lg:w-[42px] xl:w-[49px] h-auto" />
-            </span>
           </div>
-        </div>
+        </div> */}
+        {/* Desktop version */}
+        <DashedCurvePointerDesktop className="absolute top-10 lg:top-16 left-1/2 translate-x-[-60%] hidden sm:block w-full max-w-[40%] md:max-w-[30%] lg:max-w-[40%] h-[80%]" />
+
+        {/* Mobile version */}
+        <DashedCurvePointerMobile className="absolute top-0 left-0 block sm:hidden w-full max-w-[90%] xs:max-w-[50%] h-full" />
 
         {/* Content items */}
         <div
@@ -31,11 +30,11 @@ const CarrierFlowSection = ({ items }: any) => {
             <div key={index} className="flex items-start">
               <div
                 className={`flex items-start gap-8 ${
-                  item.position === "right" ? "ml-auto" : ""
+                  item.position === "right" ? "sm:ml-auto" : ""
                 }`}
               >
                 {item.position === "left" && (
-                  <div className="flex items-center gap-6 md:gap-10 lg:gap-14 max-w-[450px]">
+                  <div className="flex items-center gap-6 md:gap-10 lg:gap-14 sm:max-w-[450px]">
                     <span className="text-[62px] sm:text-[83px] md:text-[104px] lg:text-[125px] xl:text-[140px] font-bold text-[#FFFCC5] leading-none select-none font-scoutcond">
                       {item.number}
                     </span>
@@ -50,7 +49,7 @@ const CarrierFlowSection = ({ items }: any) => {
                   </div>
                 )}
                 {item.position === "right" && (
-                  <div className="flex items-center gap-6 md:gap-10 lg:gap-14 max-w-[450px]">
+                  <div className="flex items-center gap-6 md:gap-10 lg:gap-14 sm:max-w-[450px]">
                     <span className="text-[62px] sm:text-[83px] md:text-[104px] lg:text-[125px] xl:text-[140px] font-bold text-[#FFFCC5] leading-none select-none font-scoutcond">
                       {item.number}
                     </span>
