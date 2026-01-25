496d5bb64a12014d10c2edd962a9d6795e72326c
md-nafis-sadik <sadikmdnafis@gmail.com>
2026-01-25 18:23:35 +0600

    fix: discovermore animation added

--- components/products/OtherProductsCard.tsx
+++ components/products/OtherProductsCard.tsx
@@ -1,9 +1,5 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import Link from "next/link";
-import { Button } from "../ui/button";
-import { ArrowLongTailIcon } from "@/services/assets/svgs";
-import { images } from "@/services";
 
 interface IFProductCardProps {
   item: any;
@@ -13,7 +9,7 @@ const OtherProductCard = ({ item }: IFProductCardProps) => {
   return (
     <div
       className={cn(
-        "w-full min-w-[90%] 2xl:min-w-[376px] aspect-[328/418] xl:aspect-[376/460] last:aspect-[328/418] md:last:aspect-[376/240] xl:last:aspect-[376/460] last:col-span-1 md:last:col-span-2 xl:last:col-span-1 overflow-hidden bg-neutral-50 group transition_common relative",
+        "w-full min-w-[90%] 2xl:min-w-[376px] aspect-[328/418] xl:aspect-[376/460] last:aspect-[328/418] md:last:aspect-[376/240] xl:last:aspect-[376/460] last:col-span-1 md:last:col-span-2 xl:last:col-span-1 overflow-hidden bg-neutral-50 group relative animate-item3",
       )}
     >
       <Image
@@ -34,14 +30,6 @@ const OtherProductCard = ({ item }: IFProductCardProps) => {
           )}
         >
           <div className="pr-6 pl-4 pb-4 pt-5 md:pr-14 md:pl-6 md:pt-12 md:pb-6 h-full flex flex-col justify-between gap-4 md:gap-6">
-            {/* <Image
-              src={images.netroLogo}
-              alt={item?.title}
-              className="h-auto w-9 md:w-12 lg:w-14"
-              height={200}
-              width={300}
-            /> */}
-
             <div className="flex flex-col mt-auto">
               <p className="text-text-50 font-bold font-scoutcond text-[32px] xl:text-[36px] 2xl:text-[40px] leading-[100%] line-clamp-1">
                 {item?.title}
@@ -49,22 +37,6 @@ const OtherProductCard = ({ item }: IFProductCardProps) => {
               <p className="text-xs md:text-sm 2xl:text-base font-normal !leading-[1.4] text-text-200 mt-3 font-inter line-clamp-2">
                 {item?.metaDescription}
               </p>
-              {/* <p
-                className={cn(
-                  "text-[10px] md:text-xs font-normal !leading-[1.6] text-text-200 font-inter"
-                )}
-              >
-                {item?.tagList}
-              </p> */}
-              {/* 
-              <Link href={item?.url ? item?.url : `/portfolio/${item?.title}`}>
-                <Button className="w-fit group mt-3 md:mt-4 py-2">
-                  <span className="!leading-none text-xs md:text-base">
-                    See Details
-                  </span>
-                  <ArrowLongTailIcon className="h-auto w-5 md:w-6 group-hover:translate-x-2 transition_common" />
-                </Button>
-              </Link> */}
             </div>
           </div>
         </div>
--- components/projects/DiscoverMoreProjects.tsx
+++ components/projects/DiscoverMoreProjects.tsx
@@ -1,19 +1,29 @@
 "use client";
-import useEmblaCarousel from "embla-carousel-react";
-import SectionHeader from "../common/SectionHeader";
-import Autoplay from "embla-carousel-autoplay";
+
 import OtherProductCard from "../products/OtherProductsCard";
 import DescriptionHeader from "../common/DescriptionHeader";
+import { useAppearanceAnimation } from "@/hooks/useAppearanceAnimation";
 
 const DiscoverMoreProjects = ({ data }: any) => {
-  // const options = { align: "start", loop: false } as const;
-  // const [emblaRef] = useEmblaCarousel(options, [
-  //   Autoplay({ delay: 3000, stopOnInteraction: false }),
-  // ]);
+  const sectionRef = useAppearanceAnimation(".animate-item3", {
+    x: 100,
+    y: 0,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+    delay: 0.1,
+  });
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 relative bg-darkPurplebg">
-      <DescriptionHeader title="Releted Works" className="mb-6 md:mb-8 lg:mb-10" />
+    <section
+      ref={sectionRef}
+      className="w-full py-12 md:py-24 lg:py-32 relative bg-darkPurplebg"
+    >
+      <DescriptionHeader
+        title="Releted Works"
+        className="mb-6 md:mb-8 lg:mb-10"
+      />
 
       <div className="container w-full overflow-hidden">
         <div className="w-full">
